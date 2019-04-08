431bacc3d2a1f5600b8796e23285a9923714ca26
Alan-Bi <alan.bi326@gmail.com>
2019-04-08 00:01:07 -0700

    Add delete functionality

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
 const createScriptItemHTML = (scriptNumber) => {
   let scriptItem = document.createElement('div');
   scriptItem.innerHTML =
-    `<div class="script-container">
+    `<div class="script-container" id="script-container-${scriptNumber}">
       <h3 id="script-${scriptNumber}-header">Script ${scriptNumber + 1}</h3>
 
       <input type="text" id="script-${scriptNumber}-title" 
@@ -115,12 +115,44 @@ const editItemClicked = (scriptNumber) => {
     title.readOnly = true;
     script.readOnly = true;
   }
-
-  return;
 };
 
+// Possible TODO: replace with template strings to make it more
+// obviously matched with the HTML and thus more readable
 const deleteItemClicked = (scriptNumber) => {
-  return;
+  scripts.splice(scriptNumber, 1);
+  scriptStore.set('scripts', scripts);
+
+  const first = document.getElementById('script-container-' + scriptNumber);
+  first.parentNode.removeChild(first);
+
+  // Fix the script numbers that are shifted due to the removal
+  for (let i = scriptNumber; i < scripts.length; i++) {
+    const prefix = 'script-' + (i + 1) + '-';
+    const suffix = '-button-' + (i + 1);
+
+    const header = document.getElementById(prefix + 'header');
+    header.id = 'script-' + i + '-header';
+    header.innerHTML = 'Script ' + (i + 1);
+
+    const title = document.getElementById(prefix + 'title');
+    title.id = 'script-' + i + '-title';
+    // title.value = scripts[i].title;
+
+    const command = document.getElementById(prefix + 'command');
+    command.id = 'script-' + i + '-command';
+    // command.value = scripts[i].script;
+
+    document.getElementById('run' + suffix).id = 'run-button-' + i;
+    document.getElementById('edit' + suffix).id = 'edit-button-' + i;
+    document.getElementById('delete' + suffix).id = 'delete-button-' + i;
+
+    const container = document.getElementById('script-container-' + (i + 1));
+    container.id = 'script-container-' + i;
+    container.outerHTML = container.outerHTML; // Clear old event listeners
+
+    attachScriptsToItem(i);
+  }
 };
 
 const createNotification = (scriptNumber) => {
